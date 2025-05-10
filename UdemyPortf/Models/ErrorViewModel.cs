21ee80bbc60e12b0ac5378cb0b89a542afad37be
SalihSefaY <fehruan2002@homail.com>
2025-05-10 11:56:15 +0300

    Add project files.

---
+++ UdemyPortf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemyPortf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
